1bd7a58b324aa145e5d978be953ca5ea20572651
sowmy <sowmy@SOWMYA>
2022-09-14 11:55:21 -0700

    Add project files.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models
+{
+
+    //Models is a set of classes that represent the data that the app manages
+    public class TodoItem
+    {
+        public long Id { get; set; } // Id property functions as a unique key in a relational database
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
